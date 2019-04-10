8a594f7d88f11d143b58dd71f955da7108c6184c
Ankit Kumar <talk2ankit007@gmail.com>
2019-04-10 05:31:24 +0530

    Server URL path Update
    Server URL path updated with the shareable link to the .pkl file for the model.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1GCEZ4EZJ6KYNSVPMHG-26fu8dAvtpTA8'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Harmful_Flowers', 'Harmless_Flowers']
 path = Path(__file__).parent
 
 app = Starlette()
